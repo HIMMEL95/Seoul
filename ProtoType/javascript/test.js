388900760b6d958d6b96c313dc29f7e86cc80056
HIMMEL <bbluesky7738@gmail.com>
2022-08-28 08:58:20 +0900

    first

--- ProtoType/javascript/test.js
+++ ProtoType/javascript/test.js
@@ -1,26 +1,98 @@
-function validateForm() {
-    var id = document.querySelector("#id").value;
-    if (id == null || id == "") {
-        document.getElementById("id_span").innerHTML = "Id must be filled out";
-        // alert("First name must be filled out");
-        return false;
+const form = document.getElementById('form');
+const id = document.getElementById('id');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const dob = document.getElementById('dob');
+const gender = document.getElementById('gender');
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
+};
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const idValue = id.value.trim();
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+    const dobValue = dob.value.trim();
+    const genderValue = gender.value.trim();
+
+    if (idValue === '') {
+        setError(id, 'Id is required');
     } else {
-        document.getElementById("id_span").innerHTML = null;
-        return true;
+        setSuccess(id);
     }
 
-    var name = document.querySelector("#name").value;
-    if (name == null || name == "") {
-        document.getElementById("name_span").innerHTML = "Name must be filled out";
-        // alert("First name must be filled out");
-        return false;
+    if (usernameValue === '') {
+        setError(username, 'Username is required');
+    } else {
+        setSuccess(username);
     }
 
-    var email = document.querySelector("#email").value;
-    if (email == null || email == "") {
-        document.getElementById("email_span").innerHTML = "Email must be filled out";
-        // alert("First name must be filled out");
-        return false;
+    if (emailValue === '') {
+        setError(email, 'Email is required');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if (passwordValue === '') {
+        setError(password, 'Password is required');
+    } else if (passwordValue.length < 8) {
+        setError(password, 'Password must be at least 8 character.');
+    } else {
+        setSuccess(password);
+    }
+
+    if (password2Value === '') {
+        setError(password2, 'Please confirm your password');
+    } else if (password2Value !== passwordValue) {
+        setError(password2, "Passwords doesn't match");
+    } else {
+        setSuccess(password2);
+    }
+
+    if (dobValue === '') {
+        setError(dob, 'Birth is required');
+    } else {
+        setSuccess(dob);
+    }
+
+    if (genderValue === '') {
+        setError(gender, 'Birth is required');
+    } else {
+        setSuccess(gender);
     }
 
 }
+
