9fbbba6456ea19fb5bedbeabb48882f3036aa4ab
HIMMEL <bbluesky7738@gmail.com>
2022-08-27 14:00:31 +0900

    first

--- ProtoType/javascript/test.js
+++ ProtoType/javascript/test.js
@@ -1,7 +1,14 @@
 function validateForm() {
     var id = document.querySelector("#id").value;
     if (id == null || id == "") {
-        document.getElementById("id_span").innerHTML = "First name must be filled out";
+        document.getElementById("id_span").innerHTML = "Id must be filled out";
+        // alert("First name must be filled out");
+        return false;
+    }
+
+    var name = document.querySelector("#name").value;
+    if (name == null || name == "") {
+        document.getElementById("name_span").innerHTML = "Name must be filled out";
         // alert("First name must be filled out");
         return false;
     }
