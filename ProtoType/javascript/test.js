234b6187442fcf492dd65c411a41822668095ab9
HIMMEL <bbluesky7738@gmail.com>
2022-09-04 09:02:30 +0900

    2022-09-04 09:02

--- ProtoType/javascript/test.js
+++ ProtoType/javascript/test.js
@@ -14,11 +14,17 @@ checkNull = function(obj, value, message, hide) {
     }
 }
 
-checkNullWithoutFocus = function(value, message) {
+checkNullWithoutFocus = function(value, message, hide) {
     if (value == "" || value == null) {
         alert(message);
+        hide.parent().addClass('error')
+        $(hide).text(message)
+        $(hide).show()
         return false;
     } else {
+        hide.parent().removeClass('error')
+        hide.parent().addClass('success')
+        $(hide).hide()
         return true;
     }
 }
@@ -40,3 +46,27 @@ const setSuccess = element => {
     inputControl.classList.add('success');
     inputControl.classList.remove('error');
 };
+
+id_regex = function(obj) {
+    const re = /^[A-Za-z가-힣]{1}[A-Za-z가-힣]{3,19}$/g;
+    if (re.test(obj) && (obj.val() == "" || value.val() == null)) {
+        hide.parent().addClass('error')
+        $(hide).text("아이디를 입력하세요")
+        $(hide).show()
+    } else if(obj.val().length < 8) {
+        hide.parent().addClass('error')
+        $(hide).text("아이디를 8자 이상으로입력하세요")
+        $(hide).show()
+    } else {
+        hide.parent().removeClass('error')
+        hide.parent().addClass('success')
+        $(hide).hide()
+    }
+    return re.test(obj);
+}
+
+name_regex = function(obj) {
+    const re = /^[^0-9]+[가-힣]{1,4}$/g;
+    console.log(re.test(obj))
+    return re.test(obj);
+}
